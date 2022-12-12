60363743de3dd9705448bac6717a70eec4bc6b62
tmwpwo <111870494+tmwpwo@users.noreply.github.com>
2022-12-12 14:25:35 +0100

    Add files via upload

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Student struct {
+	Name string
+}
+
+func renderTemplate(w http.ResponseWriter, r *http.Request) {
+	student := Student{
+		Name: "ctf player",
+	}
+	parsedTemplate, _ := template.ParseFiles("static/static.html")
+	err := parsedTemplate.Execute(w, student)
+	if err != nil {
+		log.Println("Error executing template :", err)
+		return
+	}
+}
+
+func main() {
+	r := mux.NewRouter() // Routes consist of a path and a handler function.
+	r.HandleFunc("/", renderTemplate).Methods("GET")
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static")))
+
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe("localhost:8000", r))
+}
